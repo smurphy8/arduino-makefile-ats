cf11984d7a390d3b8726920b929a34d8c606c3e1
Scott Murphy <scottmurphy09@gmail.com>
2018-10-24 17:13:38 -0500

    Beginning moving things into Cats from DATS

---
+++ examples/BlinkInAVRC/CATS/loop.c
@@ -0,0 +1,26 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+void
+port_on_in_c () {
+PORTC = 0xFF;
+};
+
+void
+port_off_in_c () {
+PORTC = 0x00;
+};
+
+void
+delay_one_ms_in_c () {
+  _delay_ms(1);
+};
+
+void
+delay_in_c (int x) { 
+  if(x > 0) {
+   for(x;x > 0 ; --x) {
+      _delay_ms(1);
+   };
+  };
+};
